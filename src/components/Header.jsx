349fd94bb4448da67721a46898aeaa5eee983fae
Anastasiachyp <chypyhanastasia@yahoo.it>
2022-02-14 15:01:00 +0100

    add new variable employeeList

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Header } from "semantic-ui-react";
+
+const Header = () => {
+    return (
+        <Header ad="h1">Employee Management</Header>
+    )
+}
+
+export default Header
